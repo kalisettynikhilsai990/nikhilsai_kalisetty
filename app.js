7f52bbbc41200aa3719ec69e391d4c84d8b5bdd1
nikhilsai990 <kalisettynikhilsai@gmail.com>
2023-07-12 18:37:55 +0530

    my second challange

---
+++ app.js
@@ -0,0 +1,61 @@
+console.log("say hello")
+
+//their are the three keywords to declare variables in our java script
+//they are:- 1.var
+//           2.let
+//           3.const
+
+
+//1.var
+//    it can be redeclare and updated 
+// exampel of redeclare
+
+var Place1="tirupathi";
+console.log(Place1);
+
+var Place1="bangalore";
+console.log(Place1);
+
+// exampel of updated
+
+var Place2="chennai";
+console.log(Place2);
+
+    Place2="hyderabad";
+console.log(Place2);    
+
+
+//2.let
+//     it can be updated but cannot be redeclare
+// exampel of redeclare
+let Name1="nikhilsai";
+console.log(Name1);
+
+// let Name_1="sai";
+// console.log(Name_1);
+
+//exampel of updated
+
+let Name2="kalisetty";
+console.log(Name2);
+
+   Name2="sai";
+console.log(Name2);
+
+//3.const
+// it cannot be updated or redclare
+//  exampel of redeclare
+
+const City1="tirupathi";
+console.log(City1);
+
+// const City1="chennai";
+// console.log(City1);           cannot be redeclare
+
+//exampel of updated
+
+const CityName2="chennai";
+console.log(CityName2);
+
+//      CityName2="bangalore";
+// console.log(CityName2);        cannot be updated
